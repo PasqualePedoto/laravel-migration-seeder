216a9d6acc9cd5bf855a00d115d9809f39f8c093
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-20 16:51:00 +0200

    Create TrainsSeeder file to create new statements

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -9,8 +11,23 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++){
+
+            $new_train = new Train();
+            
+            $new_train->agency = $faker->word();
+            $new_train->departure_station = $faker->word();
+            $new_train->arrival_station = $faker->word();
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_code = $faker->randomNumber(5, true);
+            $new_train->carrieges_number = $faker->randomDigit();
+            $new_train->in_time = $faker->boolean();
+            $new_train->is_delete = $faker->boolean();
+            
+            $new_train->save();
+        }
     }
 }
