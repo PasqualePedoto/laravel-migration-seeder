dcaf0116f3effc6d627a65b900cc659e009a7acc
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-20 17:10:26 +0200

    commit control

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -14,11 +14,40 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    @forelse($trains as $train)
-                        <p>{{ $train->agency }}</p>
-                    @empty
-                        <p>Non ci sono treni in programmazione</p>
-                    @endforelse
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Agency</th>
+                            <th scope="col">Departure Station</th>
+                            <th scope="col">Arrival Station</th>
+                            <th scope="col">Departure Time</th>
+                            <th scope="col">Arrival Time</th>
+                            <th scope="col">Train Code</th>
+                            <th scope="col">Carrieges Number</th>
+                            <th scope="col">In Orario</th>
+                            <th scope="col">Cancellato</th>
+                        </tr>
+                        </thead>
+                        @forelse($trains as $train)
+                        <tbody>
+                            <tr>
+                                <th scope="row">{{ $train->id }}</th>
+                                <td>{{ $train->agency }}</td>
+                                <td>{{ $train->departure_station }}</td>
+                                <td>{{ $train->arrival_station }}</td>
+                                <td>{{ $train->departure_time }}</td>
+                                <td>{{ $train->arrival_time }}</td>
+                                <td>{{ $train->train_code }}</td>
+                                <td>{{ $train->carrieges_number }}</td>
+                                <td>{{ $train->in_time }}</td>
+                                <td>{{ $train->is_delete }}</td>
+                            </tr>
+                        </tbody>
+                        @empty
+                            <p>Non ci sono treni in programmazione</p>
+                        @endforelse
+                    </table>
                 </div>
             </div>
         </div>
